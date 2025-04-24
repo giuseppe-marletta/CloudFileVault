6ee4e01545938dab46b3631c7175e849c1f3dc99
giuseppe-marletta <marletta_giuseppe@outlook.com>
2025-04-24 16:40:08 +0200

    Initial commit

---
+++ auth-service/src/main/java/com/github/giuseppemarletta/auth_service/AuthServiceApplication.java
@@ -0,0 +1,24 @@
+package com.github.giuseppemarletta.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AuthServiceApplication {
+   public AuthServiceApplication() {
+   }
+
+   public static void main(String[] args) {
+      SpringApplication.run(AuthServiceApplication.class, args);
+      System.out.println("Auth Service is running...");
+   }
+
+   @GetMapping({"/hello"})
+   public String hello() {
+      return "Hello from Auth Service!";
+   }
+}
+
