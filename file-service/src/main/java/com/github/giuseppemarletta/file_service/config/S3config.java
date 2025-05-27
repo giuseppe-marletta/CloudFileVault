d0d98c28924d24e5897d016f53a8ce008849f837
giuseppe-marletta <marletta_giuseppe@outlook.com>
2025-05-27 22:42:05 +0200

    Add S3 configuration and file upload functionality
    - Implement S3Client configuration in S3config with dynamic endpoint localStack and region support.
    - Enhance FileStorageService to include file upload capability to the specified S3 bucket.
    - Update application.properties to define S3 endpoint and region.

--- file-service/src/main/java/com/github/giuseppemarletta/file_service/config/S3config.java
+++ file-service/src/main/java/com/github/giuseppemarletta/file_service/config/S3config.java
@@ -1,5 +1,44 @@
 package com.github.giuseppemarletta.file_service.config;
 
+import java.net.URI;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
+
+@Configuration
 public class S3config {
     
+    @Value("${amazon.aws.accesskey}")
+    private String awsAccessKeyId;
+
+    @Value("${amazon.aws.secretkey}")
+    private String awsSecretKey;
+
+    @Value("${amazon.s3.endpoint}")
+    private String endpoint;
+
+    @Value("${amazon.s3.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        S3ClientBuilder builder = S3Client.builder()
+                .region(Region.of(region)) 
+                .credentialsProvider(
+                    StaticCredentialsProvider.create(AwsBasicCredentials.create(awsAccessKeyId, awsSecretKey))
+                );
+        if (endpoint != null && !endpoint.isEmpty()) {
+            builder.endpointOverride(URI.create(endpoint));
+        }
+        
+        return builder.build();
+    }
+
 }
--- file-service/src/main/java/com/github/giuseppemarletta/file_service/service/FileStorageService.java
+++ file-service/src/main/java/com/github/giuseppemarletta/file_service/service/FileStorageService.java
@@ -1,5 +1,28 @@
 package com.github.giuseppemarletta.file_service.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
+@RequiredArgsConstructor
 public class FileStorageService {
     
+    private final S3Client s3Client;
+
+    @Value("${amazon.s3.bucket}")
+    private String bucketName;
+
+    public void uploadFile(String key, byte[] content) {
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        s3Client.putObject(request, RequestBody.fromBytes(content));
+    }
 }
